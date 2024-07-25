363156280ff73d8241593f0aa6a303c8883dc202
avishays-tau <157728644+avishays-tau@users.noreply.github.com>
2024-07-25 12:33:41 +0300

    update log file name

--- src/logger.h
+++ src/logger.h
@@ -14,20 +14,20 @@ enum LogLevel {
 class Logger {
 public:
     static Logger& getInstance();
-    
+
     void log(LogLevel level, const std::string& message);
     void setLogFile(const std::string& filename);
-    
+
 private:
-    Logger() : logFile("default_log.txt") {}
+    Logger() : logFile("vacuum_cleaner.log") {}
     ~Logger();
-    
+
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
-    
+
     std::ofstream logStream;
     std::string logFile;
-    
+
     void openLogFile();
     std::string getLogLevelString(LogLevel level);
 };
