a1482549ae49b68ee08ce4d8612fdbf581997571
avishays-tau <157728644+avishays-tau@users.noreply.github.com>
2024-07-28 17:49:07 +0300

    add InternalHouse base code

--- src/speedom_algorithm.h
+++ src/speedom_algorithm.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include "skelaton/abstract_algorithm.h"
 #include "sensors/battery_meter_imp.h"
 #include "sensors/walls_sensor_imp.h"
@@ -8,21 +9,34 @@
 
 class SpeedomAlgorithm : public AbstractAlgorithm {
 
-  std::size_t max_steps;
+    class InternalHouse {
+        struct tile_stats {
+            size_t dirt_level;
+            bool visited;
+            bool rechable;
+            std::vector<House::Location> neighbors;
+        };
+        std::unordered_map<House::Location, tile_stats> internal_graph;
+    public:
+        
+    };
 
-  const size_t battery_size;
-  House::Location starting_location;
 
-  const WallsSensor* walls_sensor;
-  const DirtSensor* dirt_sensor;
-  const BatteryMeter* battery_meter;
+    std::size_t max_steps;
+
+    const size_t battery_size;
+    House::Location starting_location;
+
+    const WallsSensor* walls_sensor;
+    const DirtSensor* dirt_sensor;
+    const BatteryMeter* battery_meter;
 
 public:
-  SpeedomAlgorithm();
+    SpeedomAlgorithm();
 
-  void setMaxSteps(size_t max_steps) override;
-  void setWallsSensor(const WallsSensor&) override;
-  void setDirtSensor(const DirtSensor&) override;
-  void setBatteryMeter(const BatteryMeter&) override;
-  Step nextStep() override;
+    void setMaxSteps(size_t max_steps) override;
+    void setWallsSensor(const WallsSensor&) override;
+    void setDirtSensor(const DirtSensor&) override;
+    void setBatteryMeter(const BatteryMeter&) override;
+    Step nextStep() override;
 };
