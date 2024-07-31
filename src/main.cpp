edc7805b72f89db6248bd258aaac76ed3fd63339
avishays-tau <157728644+avishays-tau@users.noreply.github.com>
2024-07-31 20:40:16 +0300

    add args parser

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,19 @@
 #include "io_handling.h"
 #include "simulator.h"
 #include "speedom_algorithm.h"
+#include "args_parser.h"
+
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        logger.log(FATAL, std::format("one argument required - inputfile, got {}", int(argc - 1)));
-    }
-
-    std::string input_file_path = argv[1];
-    std::string output_file_path = "output_" + input_file_path;
-
+    ArgsParser parser;
+    auto args = parser.parse(argc, argv);
     Simulator simulator;
-    simulator.readHouseFile(input_file_path);
-
+    simulator.readHouseFile(args.input_file_path);
+    if (args.enable_visualization) {
+        simulator.enableVisualization();
+    }
     simulator.setAlgorithm(std::make_shared<SpeedomAlgorithm>());
     simulator.run();
 
     return EXIT_SUCCESS;
-
-
 }
